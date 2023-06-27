843d8bc3d9f3991b2b00144974ef01dc03ad809d
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2023-06-27 15:22:55 +0200

    male ou femelle

--- app/models/porc.rb
+++ app/models/porc.rb
@@ -2,4 +2,5 @@ class Porc < ApplicationRecord
   belongs_to :user
   belongs_to :abattage, optional: true
   validates :user, :boucle, :boucle_mère, :boucle_père, :date_de_naissance, :sexe, presence: true
+  validates :sexe, inclusion: { in: %w[mâle femelle]}
 end

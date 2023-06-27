0b999dafe0a904c9f67f16fa4ae5b211d1b66644
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2023-06-27 14:05:22 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "porcs#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :porcs do
+    resources :prisuttus, only [:new, :create, :edit, :update]
+    resources :lonzus, only [:new, :create, :edit, :update]
+    resources :coppas, only [:new, :create, :edit, :update]
+  end
 
+  recources :abattages, only [:new, :create, :show, :edit, :update]
   # Defines the root path route ("/")
   # root "articles#index"
 end

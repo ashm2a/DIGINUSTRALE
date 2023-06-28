06eef78d82054ef1d6e04232bc527b4014b1b9ee
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2023-06-28 17:02:04 +0200

    creating product instances

--- app/controllers/lonzus_controller.rb
+++ app/controllers/lonzus_controller.rb
@@ -1,2 +1,10 @@
 class LonzusController < ApplicationController
+
+  def create
+    q_lonzu = params[:q_lonzu]
+    q_lonzu.times do
+      Lonzu.create
+    end
+  end
+
 end
--- app/controllers/prisuttus_controller.rb
+++ app/controllers/prisuttus_controller.rb
@@ -1,2 +1,10 @@
 class PrisuttusController < ApplicationController
+
+  def create
+    q_prisuttu = params[:q_prisuttu]
+    q_prisuttu.times do
+      Prisuttu.create
+    end
+  end
+
 end

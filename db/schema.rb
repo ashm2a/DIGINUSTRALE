8b111b4945bb6d7946575479ae4c90bd945be50b
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2023-06-29 17:51:59 +0200

    ajout produits to porcs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_29_153131) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_29_154514) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,10 +79,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_29_153131) do
     t.boolean "vermifuge_2_mois", default: false
     t.boolean "vermifuge_6_mois", default: false
     t.boolean "vermifuge_12_mois", default: false
-    t.integer "q_coppa", default: 0
-    t.integer "q_prisuttu", default: 0
-    t.integer "q_lonzu", default: 0
+    t.bigint "coppa_id"
+    t.bigint "prisuttu_id"
+    t.bigint "lonzu_id"
     t.index ["abattage_id"], name: "index_porcs_on_abattage_id"
+    t.index ["coppa_id"], name: "index_porcs_on_coppa_id"
+    t.index ["lonzu_id"], name: "index_porcs_on_lonzu_id"
+    t.index ["prisuttu_id"], name: "index_porcs_on_prisuttu_id"
     t.index ["user_id"], name: "index_porcs_on_user_id"
   end
 
@@ -125,6 +128,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_29_153131) do
   add_foreign_key "coppas", "porcs"
   add_foreign_key "lonzus", "porcs"
   add_foreign_key "porcs", "abattages"
+  add_foreign_key "porcs", "coppas"
+  add_foreign_key "porcs", "lonzus"
+  add_foreign_key "porcs", "prisuttus"
   add_foreign_key "porcs", "users"
   add_foreign_key "prisuttus", "porcs"
 end

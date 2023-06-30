c5c12da509ba23189416038cf5836c2bfec5768a
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2023-07-01 00:09:38 +0200

    bouton decoupe

--- app/javascript/controllers/aop_controller.js
+++ app/javascript/controllers/aop_controller.js
@@ -3,44 +3,44 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="aop"
 export default class extends Controller {
 
-  static targets = ["output", "poids", "lard", "ph", "row"]
+  static targets = ["output", "poids", "lard", "ph", "row", "decoupe"]
   connect () {
+
   }
   clickUpdatePorcs() {
     const rowElements = document.querySelectorAll('[data-aop-target="row"]');
+    //const btnDecoupeElement = document.querySelector('[data-aop-target="btn-decoupe"]');
+    const decoupeBtn = document.querySelector(".btn-decoupe a");
+    //console.log(decoupeBtn);
+
+    //decoupeBtn.classList.remove("d-none");
 
     rowElements.forEach((rowElement) => {
-      console.log(rowElement);
       const poidsValue = parseFloat(rowElement.querySelector('[data-aop-target="poids"]').value);
-      //console.log(typeof poidsValue);
-
-
       const lardValue = parseFloat(rowElement.querySelector('[data-aop-target="lard"]').value);
-      //console.log(lardValue);
       const phValue = parseFloat(rowElement.querySelector('[data-aop-target="ph"]').value);
-      //console.log(phValue);
-
       const imgElement = rowElement.querySelector('[data-aop-target="output"]');
-      //console.log(imgElement.querySelector("img"));
 
-      if(
-        ((poidsValue >= 80) && (poidsValue <= 140))
-        &&
-        ((lardValue >= 4) && (lardValue <= 6))
-        &&
-        ((phValue >= 4) && (phValue <= 6))
-      ) {
-        //console.log("AOP OK")
-        imgElement.querySelector("img").classList.remove("d-none");
-        rowElement.classList.add("bg-success");
-         }
-      else {
-        //console.log("AOP NO")
-        imgElement.querySelector("img").classList.add("d-none");
-        rowElement.classList.remove("bg-success");
-      }
-
-
+      if(poidsValue != null && lardValue != null && phValue != null)
+        decoupeBtn.classList.remove('d-none');
+        if(
+          ((poidsValue >= 80) && (poidsValue <= 140))
+          &&
+          ((lardValue >= 4) && (lardValue <= 6))
+          &&
+          ((phValue >= 4) && (phValue <= 6))
+        ) {
+          //console.log("AOP OK")
+          imgElement.querySelector("img").classList.remove("d-none");
+          rowElement.classList.add("bg-success");
+          }
+        else {
+          //console.log("AOP NO")
+          imgElement.querySelector("img").classList.add("d-none");
+          rowElement.classList.remove("bg-success");
+        }
+        end
+      end
     });
   }
 }

b9f0e3a167fe715adf5cb7509c291599180c92d9
audreysantoni <santoni.audrey@gmail.com>
2023-07-05 16:20:33 +0200

    fixing meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:nom, :prénom, :EDE,:adresse, :code_postal, presence: true])
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
